3c806ad3055db0d59ddd14dfc4fc7329c1931fe4
Oleksii Persatyi <persatyi@ukr.net>
2022-02-03 19:10:37 +0200

    10 задание

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const refs = {
+  buttonCreate: document.querySelector("[data-create]"),
+  buttonDestroy: document.querySelector("[data-destroy]"),
+  input: document.querySelector('[type="number"]'),
+  divBoxes: document.querySelector("#boxes"),
+};
+
+refs.buttonCreate.addEventListener("click", createBoxes);
+
+function createBoxes(amount) {
+  amount = refs.input.value;
+  let counter = 30;
+  const elements = [];
+  for (let i = 0; i < amount; i += 1) {
+    let newDiv = document.createElement("div");
+    newDiv.style.backgroundColor = getRandomHexColor();
+    newDiv.style.width = `${counter}px`;
+    newDiv.style.height = `${counter}px`;
+    counter += 10;
+    elements.push(newDiv);
+  }
+  refs.divBoxes.append(...elements);
+}
+
+refs.buttonDestroy.addEventListener("click", destroyBoxes);
+
+function destroyBoxes() {
+  refs.divBoxes.innerHTML = "";
 }
