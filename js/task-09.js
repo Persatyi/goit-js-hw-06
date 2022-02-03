5a49f6e817d92239f9acee0ac0751bbd8425c0f9
Oleksii Persatyi <persatyi@ukr.net>
2022-02-03 16:13:50 +0200

    задача 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const btnChangeColor = document.querySelector(".change-color");
+const colorValue = document.querySelector(".color");
+const bodyElem = document.body;
+
+btnChangeColor.addEventListener("click", () => {
+  const bodyElementColor = (bodyElem.style.backgroundColor =
+    getRandomHexColor());
+  colorValue.textContent = bodyElementColor;
+});
