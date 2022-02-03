595bb0d0dbcf5af160e3dc3c772e820d8b39c08b
Oleksii Persatyi <persatyi@ukr.net>
2022-02-03 15:31:21 +0200

    Задача 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", onFormSubmit);
 
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const inputEmail = event.target.elements.email;
+  const inputPass = event.target.elements.password;
+
+  if (!inputEmail.value.trim() || !inputPass.value.trim()) {
+    alert("Заполните все поля!");
+  } else {
+    const formData = {
+      email: inputEmail.value,
+      password: inputPass.value,
+    };
+    console.log(formData);
+    loginForm.reset();
+  }
+}
